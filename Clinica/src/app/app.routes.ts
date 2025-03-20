d665ef1ffb5ca84be0843d2f3cfb7922d45e48c6
GustavoCaste <gustavocastelluccio@gmail.com>
2025-03-20 20:43:01 -0300

    Commit inicial

---
+++ Clinica/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+
+export const routes: Routes = [
+  {path: '', component: HeaderComponent}
+];
